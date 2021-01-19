68b6681485c97375f54482f315883dc25b22e066
dhanu9209 <dhanu9209@gmail.com>
2021-01-19 17:24:13 +0530

    committed by developer 1

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 public String welcome() {
+	System.out.println("hello");
 	return "Hello World";
 }
 }
